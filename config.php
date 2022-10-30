893dcc243db19bacaefdf0e61219f5000b206bd4
Anatoli Nicolae <an@thundersquared.com>
2022-10-30 23:06:54 +0100

    Add some basic layout and clone docs
    Signed-off-by: Anatoli Nicolae <an@thundersquared.com>

--- config.php
+++ config.php
@@ -1,9 +1,31 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
     'production' => false,
     'baseUrl' => 'https://thundersquared.github.io/apiscp-notes/',
     'title' => 'ApisCP Notes',
     'description' => 'Notes and cheats for a better ApisCP administration experience.',
-    'collections' => [],
+    'collections' => [
+        'notes',
+    ],
+
+    // navigation menu
+    'navigation' => require_once('navigation.php'),
+
+    // helpers
+    'isActive' => function ($page, $path) {
+        return Str::endsWith(trimPath($page->getPath()), trimPath($path));
+    },
+    'isActiveParent' => function ($page, $menuItem) {
+        if (is_object($menuItem) && $menuItem->children) {
+            return $menuItem->children->contains(function ($child) use ($page) {
+                return trimPath($page->getPath()) == trimPath($child);
+            });
+        }
+    },
+    'url' => function ($page, $path) {
+        return Str::startsWith($path, 'http') ? $path : '/' . trimPath($path);
+    },
 ];
---
+++ navigation.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'Getting Started' => 'notes/README',
+    'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
+];
